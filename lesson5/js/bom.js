d89d3a1eba87c52d72de88954122c203e82fc7c9
Kevin Romero <kevin.romero@telus.com>
2021-05-21 18:08:25 -0600

    Created BOM exercise and updates the corresponding link in the main project page.

---
+++ lesson5/js/bom.js
@@ -0,0 +1,40 @@
+const button = document.querySelector('button');
+
+const list = document.querySelector('ul');
+
+const alert_container = document.querySelector('main');
+
+
+let add = () => {
+  const li = document.createElement('li');
+      li.innerHTML = `
+      <b>${document.querySelector('#favchap').value}</b>
+      <a href="#" class="btn btn-danger btn-sm delete" >X</a>
+      `;
+  // Append the row to the list
+  list.appendChild(li);
+  document.querySelector('#favchap').value = "";
+  document.querySelector('#favchap').focus();
+
+  li.addEventListener('click', (e) => {
+    li.remove(e.target);
+  });
+
+}
+
+button.addEventListener('click', () => {
+  const input = document.querySelector('#favchap').value;
+  if (input === "") {
+    const alert = document.createElement('div');
+        alert.innerHTML = `
+        <div class="alert">Please enter a chapter of The Book of Mormon</div>
+        `;
+        // Append the row to the list
+        alert_container.appendChild(alert);
+        document.querySelector('#favchap').focus();
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  }
+  else {
+    add();
+  }
+});
