cde448c17313930798c9abb2ad563740b12b85f2
Kevin Romero <kevin.romero@telus.com>
2021-06-02 14:35:52 -0600

    Implemented font loader and wind chill factor calculation script.

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -1,7 +1,40 @@
-function functionDisplay() {
-    document.querySelector(".year").innerHTML = new Date().getFullYear();
-    document.querySelector(".date").innerHTML = document.lastModified;
+const monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+"Friday", "Saturday"];
+
+const date = new Date();
+const dayName = dayNames[date.getDay()];
+const monthName = monthNames[date.getMonth()];
+const year = date.getFullYear();
+const currentDate = date.getDate();
+
+function functionDisplayDate() {
+
+    const formattedCurrentDate = `${dayName}, ${currentDate} ${monthName} ${year}`;
+
+    document.querySelector('.date').textContent = formattedCurrentDate;
+    document.querySelector(".year").innerHTML = year;
 }
+
+function displayAnnoucement(){
+  if (dayName === "Friday"){
+    const announcement = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    document.querySelector('.announcement').style.display = 'block';
+    document.querySelector('.announcement').innerHTML = announcement;
+  }
+}
+
+function getWindChill(temperature, windSpeed) {
+  return 35.74 + (0.6215 * temperature) + (0.4275 * temperature - 35.75)  *  windSpeed ^ 0.16;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    functionDisplay();
+  const temperature = document.querySelector('.temp').innerText;
+  const wind = document.querySelector('.wind').innerText;
+  const chill = document.querySelector('.chill');
+  functionDisplayDate();
+  displayAnnoucement();
+  chill.innerHTML = `<b>${getWindChill(temperature, wind)} &deg; F</b>`;
 });
